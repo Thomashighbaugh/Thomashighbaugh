0ad12e9f88f77abdeb30f462ffb9704ec80619ef
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2024-01-22 06:30:30 -0800

    recents

--- src/replacers/recentworks.js
+++ src/replacers/recentworks.js
@@ -3,11 +3,11 @@ const { getRepositories } = require('../utils/githubApi');
 module.exports = async function (data) {
   const repos = await getRepositories(data.user);
 
-  const getRecents = (a, b) => (new Date(a.updated_at) >= new Date(b.updated_at) ? -1 : 1);
+  const getRecents = (a, b) => (new Date(a.updated_at) > new Date(b.updated_at) ? -1 : 1);
   const formatJson = (elem) =>
     `<a href="${
       elem.html_url
-    }"><img height="125px" min-height="125px" max-height="125px" width="40%" margin-top=0 margin-bottom=0 margin-right="5%" margin-left=0 src="https://github-readme-stats-server.vercel.app/api/pin/?username=${
+    }"><img style="height: 125px; min-height:125px; max-height:125px; width: 40%; margin-top:0; margin-bottom:0; margin-right:5%; margin-left:0;" src="https://github-readme-stats-server.vercel.app/api/pin/?username=${
       data.user
     }&repo=${encodeURIComponent(
       elem.name,
