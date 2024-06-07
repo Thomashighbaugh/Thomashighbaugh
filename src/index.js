b70b68c94c888b182c78f59df3df1ddb898f9e3d
Thomas Highbaugh <thighbaugh@zoho.com>
2024-06-07 12:15:43 -0700

    chore: Update README.md with values from replacers

--- src/index.js
+++ src/index.js
@@ -1,62 +1,26 @@
-// Import required modules
-const fs = require('fs');
-const data = require('./data.js');
-
-/**
- * Read all the replacers from the replacers directory and create an object
- * with the replacer name as the key and the replacer function as the value.
- * @returns {Object} An object containing all the replacer functions
- */
-const replacers = fs
-  .readdirSync('./src/replacers')
-  .filter((file) => file.endsWith('js'))
-  .reduce((acc, cur) => {
-    acc[cur.slice(0, -3)] = require('./replacers/' + cur);
-    return acc;
-  }, {});
-
 /**
  * Update the README.md file with the values from the replacers.
  */
 (async function updateREADME() {
   // Read the README_TEMPLATE.md file
-  fs.readFile('README_TEMPLATE.md', 'utf-8', async (err, content) => {
-    if (err) {
-      throw err;
-    }
+  const content = (await fs.promises.readFile('README_TEMPLATE.md', 'utf-8'))
+    .split(/\r?\n/);
 
-    // Create a regular expression to match all the replacer tags in the README_TEMPLATE.md file
-    const replacementRegex = new RegExp(`%{(${Object.keys(replacers).join('|')})}`, 'gm');
-
-    /**
-     * Replace all the replacer tags in the content string with the values returned by the replacer functions.
-     * @param {string} str - The string to replace the replacer tags in.
-     * @param {RegExp} regex - The regular expression to match the replacer tags.
-     * @param {function} asyncFn - The function to execute for each replacer tag.
-     * @returns {string} - The string with the replacer tags replaced with the values returned by the replacer functions.
-     */
-    async function replaceAsync(str, regex, asyncFn) {
-      const promises = [];
-      str.replace(regex, (match, ...args) => {
-        const promise = asyncFn(match, ...args);
-        promises.push(promise);
-      });
-      const data = await Promise.all(promises);
-      return str.replace(regex, () => data.shift());
-    }
-
-    // Replace all the replacer tags in the content string with the values returned by the replacer functions
-    const updatedMd = await replaceAsync(content, replacementRegex, async (e) => {
-      return await replacers[e.slice(2, -1)](data);
-    });
-
-    // Write the updated content to the README.md file
-    fs.writeFile('README.md', updatedMd, 'utf-8', (err) => {
-      if (err) {
-        throw err;
+  // Create a regular expression to match all the replacer tags in the README_TEMPLATE.md file
+  const replacementRegex = new RegExp(`%{(${Object.keys(replacers).join('|')})}`, 'gm');
+  const replacements = await Promise.all(
+    content.map(async (line) => {
+      if (replacementRegex.test(line)) {
+        return await replacers[line.match(replacementRegex)[1].slice(2, -1)](data);
       }
-      console.log('README update complete.');
-    });
-  });
+      return line;
+    }),
+  );
+
+  // Write the updated content to the README.md file
+  await fs.promises.writeFile('README.md', replacements.join('\n'));
+  console.log('README update complete.');
 })();
+
+
 
