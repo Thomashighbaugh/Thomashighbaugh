c44660710b136e6d86e4d594ecde9a168c9d8d70
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2025-02-12 01:27:59 -0800

    2025-02-12 01:27:59

--- src/utils/drawFollowerImage.js
+++ src/utils/drawFollowerImage.js
@@ -1,4 +1,5 @@
-import Jimp from 'jimp';
+const Jimp = require('jimp');
+const dateStringifier = require('date-stringifier');
 
 async function drawFollowerImage(follower, fileName) {
   const avatar = await Jimp.read(follower.avatar_url);
@@ -10,17 +11,16 @@ async function drawFollowerImage(follower, fileName) {
   const textFont = await Jimp.loadFont('./src/resources/fonts/Coder30.fnt');
   const titleFont = await Jimp.loadFont('./src/resources/fonts/Coder48.fnt');
   const dateFont = await Jimp.loadFont('./src/resources/fonts/Coder18.fnt');
-  const formattedDate = dateStringifier(new Date(follower.created_at), `Joined {DD}/{MM}/{yy}`);
+  const formattedDate = dateStringifier(new Date(follower.created_at), `Joined {MM}/{DD}/{yy}`);
 
   base
     .composite(avatar, 18, 16)
     .print(textFont, 440, 123, follower.public_repos)
     .print(textFont, 510, 159, follower.followers)
     .print(dateFont, 670, 220, formattedDate)
-
     .print(titleFont, 430, 33, follower.login);
 
   await base.writeAsync(`./src/resources/images/${fileName}`);
 }
 
-export { drawFollowerImage };
+module.exports = { drawFollowerImage };
