505648d66a6c1f41b9dbbf554563d61596482e63
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2025-02-12 07:38:14 -0800

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -2,8 +2,8 @@ module.exports = {
   colors: {
     primary: '#3c3c3c',
     lightPrimary: '#69676c',
-    secondary: '#FC618D',
-    lightSecondary: '#6AB0F3',
+    secondary: '#6AB0F3',
+    lightSecondary: '#FC618D',
     white: '#F2f2f2',
     black: '#1c1c1c',
   },
